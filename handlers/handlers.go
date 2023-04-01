18c39da57aeac8d0659efa2d0a39ef9fa439dac9
nonokaM <c1201071@gmail.com>
2023-04-01 17:03:24 +0900

    add handler functionality

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -4,6 +4,9 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
 )
 
 //  Define handler
@@ -17,11 +20,29 @@ func PostArticleHandler(w http.ResponseWriter, req *http.Request) {
 }
 
 func ArticleListHandler(w http.ResponseWriter, req *http.Request) {
-	io.WriteString(w , "Article List...\n")
+	queryMap := req.URL.Query()
+
+	var page int
+	if p, ok := queryMap["page"]; ok && len(p) > 0 {
+		var err error
+		page, err = strconv.Atoi(p[0])
+		if err != nil {
+			http.Error(w, "Invalid query paremeter", http.StatusBadRequest)
+			return
+		}
+	} else {
+		page = 1
+	}
+	resString := fmt.Sprintf("Article List (page %d)\n", page)
+	io.WriteString(w , resString)
 }
 
 func ArticleDetailHandler(w http.ResponseWriter, req *http.Request) {
-	articleID :=1
+	articleID, err := strconv.Atoi(mux.Vars(req)["id"])
+	if err != nil {
+		http.Error(w, "Invalid query parameter", http.StatusBadRequest)
+	return
+	}
 	resString := fmt.Sprintf("Article No.%d\n", articleID)
 	io.WriteString(w, resString)
 }
