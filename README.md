5abec52b3d01529276c7bea6f7318ec03e4e9a14
nonokaM <c1201071@gmail.com>
2023-08-27 00:10:03 +0900

    add: select処理

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 
 クエリを実行するためのコマンド
 mysql -h 127.0.0.1 -u docker sampledb -p < insertData.sql
+
+「Goでの DB 処理は全て sql.DB 型を通してやる」の原則
--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"database/sql"
+	"github.com/NonokaM/Go-API/models"
 	"fmt"
 
 	_ "github.com/go-sql-driver/mysql"
@@ -24,9 +25,41 @@ func main() {
 	defer db.Close()
 
 	// sql.DB型のPingメソッドで疎通確認
-	if err := db.Ping(); err != nil {
+	// if err := db.Ping(); err != nil {
+	// 	fmt.Println(err)
+	// } else {
+	// 	fmt.Println("connect to DB")
+	// }
+
+	const sqlStr = `
+		select title, contents, username, nice
+		from articles;
+	`
+
+	// Queryメソッドに実行したいSQLSQL, 戻り値rowsに実行結果
+	rows, err := db.Query(sqlStr)
+	if err != nil {
 		fmt.Println(err)
-	} else {
-		fmt.Println("connect to DB")
+		return
 	}
+	defer rows.Close()
+
+	articleArray := make([]models.Article, 0)
+	// rowsの戻り値がtrueの間
+	// rows に存在するレコードそれぞれに対して、繰り返し処理を実行する
+	for rows.Next() {
+	// 変数 article の各フィールドに、取得レコードのデータを入れる
+	// (SQL クエリの select 句から、タイトル・本文・ユーザー名・いいね数が返ってくることはわかっている)
+		var article models.Article
+		err := rows.Scan(&article.Title, &article.Contents, &article.UserName, &article.NiceNum)
+
+		if err != nil {
+			fmt.Println(err)
+		} else {
+			// 読み出し結果を格納した変数 article を、配列に追加
+			articleArray = append(articleArray, article)
+		}
+	}
+
+	fmt.Printf("%+v\n", articleArray)
 }
