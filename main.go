23f0f3c5fc90f1c310893e75b9eaf4cc451d57f3
nonokaM <c1201071@gmail.com>
2023-08-28 23:57:54 +0900

    add: クエリにSQLを埋め込む

--- main.go
+++ main.go
@@ -12,54 +12,37 @@ func main() {
 	dbUser := "docker"
 	dbPassword := "docker"
 	dbDatabase := "sampledb"
-
-	// DBに接続するためのアドレス文を定義
 	dbConn := fmt.Sprintf("%s:%s@tcp(127.0.0.1:3306)/%s?parseTime=true", dbUser, dbPassword, dbDatabase)
 
-	// Open関数を用いてDBに接続
 	db, err := sql.Open("mysql", dbConn)
 	if err != nil {
 		fmt.Println(err)
 	}
-	// プログラムが終了するときに、コネクションがCloseされるようにする
 	defer db.Close()
 
-	// sql.DB型のPingメソッドで疎通確認
-	// if err := db.Ping(); err != nil {
-	// 	fmt.Println(err)
-	// } else {
-	// 	fmt.Println("connect to DB")
-	// }
-
+	articleID := 1000
 	const sqlStr = `
-		select title, contents, username, nice
-		from articles;
+		select *
+		from articles
+		where article_id = ?;
 	`
+	row := db.QueryRow(sqlStr, articleID)
+	if err := row.Err(); err != nil {
+		fmt.Println(err)
+		return
+	}
 
-	// Queryメソッドに実行したいSQLSQL, 戻り値rowsに実行結果
-	rows, err := db.Query(sqlStr)
+	var article models.Article
+	var createdTime sql.NullTime
+	err = row.Scan(&article.ID, &article.Title, &article.Contents, &article.UserName, &article.NiceNum, &createdTime)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-	defer rows.Close()
 
-	articleArray := make([]models.Article, 0)
-	// rowsの戻り値がtrueの間
-	// rows に存在するレコードそれぞれに対して、繰り返し処理を実行する
-	for rows.Next() {
-	// 変数 article の各フィールドに、取得レコードのデータを入れる
-	// (SQL クエリの select 句から、タイトル・本文・ユーザー名・いいね数が返ってくることはわかっている)
-		var article models.Article
-		err := rows.Scan(&article.Title, &article.Contents, &article.UserName, &article.NiceNum)
-
-		if err != nil {
-			fmt.Println(err)
-		} else {
-			// 読み出し結果を格納した変数 article を、配列に追加
-			articleArray = append(articleArray, article)
-		}
+	if createdTime.Valid {
+		article.CreatedAt = createdTime.Time
 	}
 
-	fmt.Printf("%+v\n", articleArray)
+	fmt.Printf("%+v\n", article)
 }
